f3b27b37cbe87e4237b0b38674b37dfd6451f633
joaoguilherme222 <martinslino1603gt@gmail.com>
2024-10-25 11:47:51 -0300

    feat-8 #19 criação página manutenção

--- src/views/PaginaManutencao.vue
+++ src/views/PaginaManutencao.vue
@@ -1,7 +1,30 @@
 <script setup>
+import Menu from '@/components/Menu.vue';
+import Footer from '@/components/Footer.vue';
 </script>
 <template>
-        ola mundo.
+<header>
+        <Menu></Menu>
+</header>
+<div class="container">
+        <h1 class="title">Manutenção</h1>
+        <div class="description">
+
+            <p class="t2">
+                Oferecemos soluções completas para a manutenção de sua casa,
+                assegurando que todos os aspectos estejam em perfeito estado
+                de funcionamento. Nossa equipe é especializada em resolver uma variedade de problemas,
+                 garantindo segurança e eficiência em cada intervenção.
+            </p>
+        </div>
+
+        <h2 class="subtitle"></h2>
+
+
+
+
+        <Footer></Footer>
+</div>
 </template>
 <style scoped>
 </style>
