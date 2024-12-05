fd8e9972358e799f2388afa2036ce12e2dec6a6c
Lucas Bokorni <131266284+bokorni@users.noreply.github.com>
2024-12-05 13:49:36 -0300

    Update user.js

--- src/stores/user.js
+++ src/stores/user.js
@@ -1,51 +1,31 @@
-import { defineStore  } from 'pinia'
+import { defineStore } from 'pinia'
 
 export const useUserStore = defineStore('user', {
   state: () => ({
-    isLoggedIn: false,
     user: null
   }),
 
   actions: {
     setUser(name, email, password) {
-
       const user = { name, email, password }
-  
-      this.isLoggedIn = true
+      this.user = user
       localStorage.setItem('user', JSON.stringify(user))
-
-      const user = { name, email, password };
-      this.user = user;
-      localStorage.setItem('user', JSON.stringify(user));
-
     },
-  
+
     loadUser() {
-      const storedUser = localStorage.getItem('user');
+      const storedUser = localStorage.getItem('user')
       if (storedUser) {
-        this.user = JSON.parse(storedUser);
+        this.user = JSON.parse(storedUser)
       }
     },
-  
+
     clearUser() {
-      this.user = null;
-    },
-
-
-    getters: {
-      isLoggedIn: (state) => !!state.user
-    },
-    
-    logout() {
       this.user = null
-      this.isLoggedIn = false
     },
-
 
     deleteUserData() {
-      this.clearUser();
+      this.user = null
+      localStorage.removeItem('user')
     }
   }
 })
-
- 
